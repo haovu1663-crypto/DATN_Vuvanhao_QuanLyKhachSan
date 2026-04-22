b13b1ec67a8607a3a3aacab6b346f0533cbf8c80
haovu1663-crypto <haovu1663@gmail.com>
2026-04-23 00:39:48 +0700

    buôi 1

---
+++ src/main/java/re/quanlykhachsan/QuanLyKhachSanApplication.java
@@ -0,0 +1,13 @@
+package re.quanlykhachsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyKhachSanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyKhachSanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/re/quanlykhachsan/QuanLyKhachSanApplicationTests.java
@@ -0,0 +1,13 @@
+package re.quanlykhachsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyKhachSanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
